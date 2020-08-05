8b63cb9502306ec38dad6a9f5bc278f3176f4079
Sushant Kaushal <sushantkaushal619@gmail.com>
2020-08-05 16:43:36 +0530

    it is final update..Nothing will change now

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
         // Welcome the user to the room
         socket.emit('message', generateMessage('Admin','Welcome!'))
         // Broadcast an event to everyone in the room
-        socket.broadcast.to(user.room).emit('message', generateMessage(user.username, 'has joined!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage(user.username, ' has joined!'))
         
         io.to(user.room).emit('roomData',{
             room:user.room,
@@ -58,7 +58,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect',()=>{
         const user=removeUser(socket.id)
         if(user){
-            io.to(user.room).emit('message',generateMessage('Admin',`${user.username}has left!!`))
+            io.to(user.room).emit('message',generateMessage(user.username,' has left!!'))
             io.to(user.room).emit('roomData',{
                 room:user.room,
                 users:getUsersInRoom(user.room)
